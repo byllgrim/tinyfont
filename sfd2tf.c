836475944e69a164d3f8375600e5649ce3241c2d
byllgrim <byllgrim@github.com>
2016-04-27 19:04:06 +0200

    implement parseheader

--- sfd2tf.c
+++ sfd2tf.c
@@ -1,25 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* Function declarations */
+static void parseheader();
+static void parseglyphs();
+
+/* Global variables */
+static char *copyright = NULL;
+static int ascent = -1;
+static int descent = -1;
+
+/* Function definitions */
+void
+parseheader()
+{
+	char *key, *end, *str = calloc(BUFSIZ, sizeof(char));
+
+	/* TODO is this a bad way to indicate uninitialized state? */
+	while(descent == -1 || ascent == -1 || copyright == NULL) {
+		fgets(str, BUFSIZ, stdin);
+		key = strtok_r(str, ": ", &end);
+
+		if(!strcmp(key, "Copyright")) {
+			copyright = malloc(strlen(++end) - 1);
+			strcpy(copyright, strtok(end, "\n")); //TODO strncpy?
+		}
+		if(!strcmp(key, "Ascent"))
+			ascent = atoi(++end);
+		if(!strcmp(key, "Descent"))
+			descent = atoi(++end);
+	}
+}
+
+void
+parseglyphs()
+{
+	/* parse glyphs:
+	 *   'StartChar: '
+	 *     ∟> 'Fore\nSplineSet'
+	 *     ∟> 'EndSplineSet'
+	 */
+}
 
 int
 main()
 {
-	char *str = calloc(BUFSIZ, sizeof(char));
+	parseheader();
+	parseglyphs();
 
+	char *str = malloc(BUFSIZ);
 	while(!feof(stdin)) {
-		printf("%s", str);
 		fgets(str, BUFSIZ, stdin);
-
-		/* parse header:
-		 *   'Copyright: '
-		 *   'Ascent: '
-		 *   'Descent: '
-		 *
-		 * parse glyphs:
-		 *   'StartChar: '
-		 *     ∟> 'Fore\nSplineSet'
-		 *     ∟> 'EndSplineSet'
-		 */
 	}
 
 	return 0;
