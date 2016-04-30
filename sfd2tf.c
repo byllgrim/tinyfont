63975b888ef4daa7d286d0ff2701be7e259c4e32
byllgrim <byllgrim@github.com>
2016-04-30 14:57:55 +0200

    write glyphmap to new format

--- sfd2tf.c
+++ sfd2tf.c
@@ -13,26 +13,27 @@ struct Command {
 	float x0, y0;
 	float x1, y1;
 	float x2, y2;
-	float x3, y3;
 	int type;
 	Command *next;
 };
 
 typedef struct Glyph Glyph;
 struct Glyph {
-	int codepoint; //TODO uint16_t ?
+	int codepoint; /* TODO uint16_t ? */
 	int width;
+	int length; /* length in bytes */
 	Command *commands;
 	Glyph *next;
 };
 
 /* Function declarations */
+static void *ecalloc(size_t nmemb, size_t size);
 static void parse();
 static void parseglyph();
 static int movetocommands();
-static Command *parsecommands();
+static int parsecommands(Glyph *glyph);
 static void writeheader();
-static void writeglyphs();
+static void writemap();
 
 /* Global variables */
 static char *copyright;
@@ -44,6 +45,16 @@ static Glyph *lastglyph;
 static int glyphcount = 0;
 
 /* Function definitions */
+void *
+ecalloc(size_t nmemb, size_t size)
+{
+	void *p;
+
+	if (!(p = calloc(nmemb, size)))
+		perror(NULL);
+	return p;
+}
+
 void
 parse()
 {
@@ -53,7 +64,7 @@ parse()
 	if (!strcmp(key, "StartChar"))
 		parseglyph();
 	else if (!strcmp(key, "Copyright"))
-		strcpy(copyright, strtok(end+1, "\n")); //TODO strncpy?
+		strncpy(copyright, strtok(end+1, "\n"), BUFSIZ);
 	else if (!strcmp(key, "Ascent"))
 		ascent = atoi(++end);
 	else if (!strcmp(key, "Descent"))
@@ -63,7 +74,8 @@ parse()
 void
 parseglyph()
 {
-	Glyph *glyph = malloc(sizeof(Glyph));
+	Glyph *glyph = ecalloc(1, sizeof(Glyph));
+	glyph->length = 6; /* sizeof glyph header */
 	char *end;
 
 	fgets(strbuf, BUFSIZ, stdin);
@@ -75,18 +87,17 @@ parseglyph()
 	glyph->width = atoi(end);
 
 	if (movetocommands())
-		glyph->commands = parsecommands();
+		glyph->length += parsecommands(glyph);
 
 	lastglyph->next = glyph;
 	lastglyph = glyph;
 	glyphcount++;
-	//TODO glyphsize += commandsize?
 }
 
 int
 movetocommands()
 {
-	char *prev = malloc(BUFSIZ);
+	char *prev = ecalloc(1, BUFSIZ);
 
 	while (strcmp(prev, "Fore\n") || strcmp(strbuf, "SplineSet\n")) {
 		if (!strcmp(strbuf, "EndChar\n")) {
@@ -103,56 +114,54 @@ movetocommands()
 	return 1;
 }
 
-Command *
-parsecommands()
+int
+parsecommands(Glyph *glyph)
 {
 	char *line;
-	float x0, y0;
-	Command *first = malloc(sizeof(Command)); //TODO dummies use memory
+	Command *first = ecalloc(1, sizeof(Command)); /* TODO dummies/memory */
 	Command *last = first;
+	int length = 0;
 
 	while (strcmp(strbuf, "EndSplineSet\n")) {
+		Command *cmd = ecalloc(1, sizeof(Command));
 		line = strbuf;
 		float x = strtof(line, &line);
 		float y = strtof(line, &line);
 		char *t = strtok_r(line, " ", &line);
 
 		if (!strcmp(t, "m")) {
-			x0 = x; y0 = y;
+			cmd->x0 = x; cmd->y0 = y;
+			cmd->type = moveto;
+			length += 12; /* sizeof moveto */
 		} else if (!strcmp(t, "l")) {
-			Command *cmd = malloc(sizeof(Command));
-			cmd->x0 = x0; cmd->y0 = y0;
-			cmd->x3 = x; cmd->y3 = y;
-			last->next = cmd;
-			last = cmd;
-			x0 = x; y0 = y;
-			//TODO commandsize += 2?
+			cmd->x0= x; cmd->y0 = y;
+			cmd->type = lineto;
+			length += 12; /* sizeof lineto */
 		} else {
-			Command *cmd = malloc(sizeof(Command));
-			cmd->x0 = x0; cmd->y0 = y0;
-			cmd->x1 = x; cmd->y1 = y;
-			cmd->x2 = strtof(t, &t); cmd->y2 = strtof(line, &line);
-			cmd->x3 = strtof(line, &line);
-			cmd->y3 = strtof(line, &line);
-			last->next = cmd;
-			last = cmd;
-			x0 = cmd->x3; y0 = cmd->y3;
-			//TODO commandsize += 4?
+			cmd->x0 = x; cmd->y0 = y;
+			cmd->x1 = strtof(t, &t); cmd->y1 = strtof(line, &line);
+			cmd->x2 = strtof(line, &line);
+			cmd->y2 = strtof(line, &line);
+			cmd->type = curveto;
+			length += 28; /* sizeof curveto */
 		}
 
+		last->next = cmd;
+		last = cmd;
 		fgets(strbuf, BUFSIZ, stdin);
 	}
 
-	return first;
+	glyph->commands = first;
+	return length;
 }
 
 void
 writeheader()
 {
-	fwrite("tinyfont", 1, 8, stdout); //TODO consider printf
+	fwrite("tinyfont", 1, 8, stdout);
 
 	uint16_t length = (uint16_t)strlen(copyright);
-	uint16_t n_length = htons(length); //TODO this assumes LE
+	uint16_t n_length = htons(length); /* TODO this assumes LE */
 	fwrite(&n_length, sizeof(uint16_t), 1, stdout);
 	fwrite(copyright, 1, length, stdout);
 
@@ -162,32 +171,34 @@ writeheader()
 }
 
 void
-writeglyphs()
+writemap()
 {
 	int i;
-	uint16_t maplength, n_maplength, codepoint, offset;
-	Glyph *glyph = firstglyph->next; //first is dummy
+	uint16_t maplength, n_maplength, codepoint, offset, n_offset;
+	Glyph *glyph = firstglyph->next; /* first is dummy */
 
-	maplength = (uint16_t)(glyphcount * 4); //4 = bytes/entry
+	maplength = (uint16_t)(glyphcount * 4); /* 4 = bytes/entry */
 	n_maplength = htons(maplength);
 	fwrite(&n_maplength, sizeof(uint16_t), 1, stdout);
 
 	for (offset = i = 0; i < glyphcount; i++) {
-		fprintf(stderr, "codepoint: %d\n", glyph->codepoint);
 		codepoint = htons((uint16_t)glyph->codepoint);
 		fwrite(&codepoint, sizeof(uint16_t), 1, stdout);
-		fwrite(&offset, sizeof(uint16_t), 1, stdout);
-		//TODO offset += glyph->length;
-		glyph = glyph->next; //TODO fix segfault
+
+		n_offset = htons(offset);
+		fwrite(&n_offset, sizeof(uint16_t), 1, stdout);
+		offset += glyph->length;
+
+		glyph = glyph->next;
 	}
 }
 
 int
 main()
 {
-	copyright = malloc(BUFSIZ); /* TODO init()? */
-	strbuf = malloc(BUFSIZ);
-	firstglyph = malloc(sizeof(Glyph)); //TODO dummies use memory
+	copyright = ecalloc(1, BUFSIZ);
+	strbuf = ecalloc(1, BUFSIZ);
+	firstglyph = ecalloc(1, sizeof(Glyph)); /* TODO dummies use memory */
 	lastglyph = firstglyph;
 
 	while(!feof(stdin)) {
@@ -196,8 +207,9 @@ main()
 	}
 
 	writeheader();
-	writeglyphs();
+	writemap();
+	//writeglyphs();
 
-	//TODO does one free buffers before return?
+	/* TODO does one free buffers before return? */
 	return 0;
 }
