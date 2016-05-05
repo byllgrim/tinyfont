bdd15cc75287c1d83e9a8a46efa5ee609bafd6b3
byllgrim <byllgrim@github.com>
2016-05-05 10:48:28 +0200

    added -Wextra -Os

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 LIBS = -lutf
 
 CPPFLAGS = -D_POSIX_C_SOURCE=200112L
-CFLAGS = -std=c89 -pedantic-errors -Wall ${CPPFLAGS}
+CFLAGS = -std=c89 -pedantic-errors -Wall -Wextra -Os ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 
 CC = cc
